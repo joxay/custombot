dbd076042ed213dec5edb77e203bf5450718077f
joxay <jothxay@gmail.com>
2024-10-18 16:38:32 +0200

    timeout

--- autoTempChannel/mycog.py
+++ autoTempChannel/mycog.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import commands, app_commands
 import polling2, time
 from opgg.opgg import OPGG
 from opgg.summoner import Summoner
@@ -9,7 +9,7 @@ class MyCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        polling2.poll(target=time.time, step=0.5, timeout=6, poll_forever=True, step_function=self.pollStep())
+        polling2.poll(target=time.time, step=30, timeout=6, poll_forever=True, step_function=self.pollStep())
         
 
     @commands.command()
@@ -17,6 +17,8 @@ class MyCog(commands.Cog):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
+    
+
 
     async def pollStep():
         #Check
