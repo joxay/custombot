02c3f4a92c6ef65f5d3696b22bfa9c1466bea28d
joxay <jothxay@gmail.com>
2024-10-18 17:05:21 +0200

    test

--- autoTempChannel/mycog.py
+++ autoTempChannel/mycog.py
@@ -19,7 +19,7 @@ class MyCog(commands.Cog):
         await ctx.send("I can do stuff!")
         opgg_obj = OPGG()
 
-        summoner: Summoner = opgg_obj.search("BettaZero#EUW")
+        summoner: Summoner = opgg_obj.search("Doublelift#NA1")
         ctx.send(summoner)
 
     
